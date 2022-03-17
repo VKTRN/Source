56238cade58d2a1f1f4897d6901b0d7cb9c54ca1
VKTRN <vktr.ndns@gmail.com>
2022-03-17 14:59:27 +0100

    add HOC-branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import {useRef} from "react"
 import './scss/custom.scss';
+import ClickCounter from "./components/ClickCounter";
+import HoverCounter from "./components/HoverCounter";
 
 function App() {
 
   return (
     <div className="App">
+      <ClickCounter></ClickCounter>
+      <HoverCounter></HoverCounter>
     </div>
   );
 }
